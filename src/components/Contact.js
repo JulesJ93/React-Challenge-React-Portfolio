d8bc5c2ef42457a72bfa61158d54c501dd35d88d
Jules Joseph <julesj93@gmail.com>
2022-12-08 15:58:13 -0500

    editing tabs

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p> <ol>
+               <li>Phone-508-818-1881</li> 
+               <li> Email- JulesJ93@gmail.com </li>
+               <li> Linkedin-https://www.linkedin.com/in/jules-joseph-b27a22104</li>
+               <li>Github-https://github.com/JulesJ93</li>
+            </ol>
+            </p>
+    </div>
+  );
+}
