9b455b7037ff7f1981e9648555afc4a1b08067fa
Jules Joseph <julesj93@gmail.com>
2022-12-08 17:17:24 -0500

    added image in about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,9 @@ export default function About() {
     <div>
       <p className="content is-medium">From Sales to Full Stack Developer</p>
       <hr />
-      <img className="my-pic" src={process.env.PUBLIC_URL + '/img/profile.jpg'} alt="Jules Joseph"/>
+      <img className="my-pic" src={process.env.PUBLIC_URL + '/IMG_0022.jpg'} alt="Jules Joseph"/>
       <p className="content is-italic mt-4">
-      Hello, my name is Jules Joseph I am a web developer from Brockton,MA and currently reside in West New York, New Jersey.
-      </p>
+      Hello, my name is Jules Joseph I am a web developer from Brockton,MA currently residing in West New York, New Jersey. I have a strong passion for collaboration and creativity and most importantly continued learning.    </p>
     </div>
   );
 }
