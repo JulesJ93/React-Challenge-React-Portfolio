b59923a3ecfe55030d3d6fa09bcb9d71ded4c142
Jules Joseph <julesj93@gmail.com>
2022-12-08 15:58:39 -0500

    adding about info

--- src/components/About.js
+++ src/components/About.js
@@ -9,12 +9,6 @@ export default function About() {
       <p className="content is-italic mt-4">
       Hello, my name is Jules Joseph I am a web developer from Brockton,MA and currently reside in West New York, New Jersey.
       </p>
-      <p className="content">
-        I always act as a product manager for learning resources, which guides
-        users to what they need in the moment, but also enable them to explore
-        content on their terms using video, audio, documentation, and so much
-        more in an advanced learning ecosystem.
-      </p>
     </div>
   );
 }
